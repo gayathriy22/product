24139f97ac9c40762282397c9e3e60280ce4e81e
Gayathri Yedavilli <gyedavil@uci.edu>
2024-04-03 13:25:18 -0700

    edit tasks

--- src/app/dummyDashboard/page.tsx
+++ src/app/dummyDashboard/page.tsx
@@ -2,8 +2,8 @@
 import { UserInfoResponse } from "@/lib/google-oauth2";
 import { FormEvent, useEffect, useState } from "react";
 import { Box, Text, Link } from '@chakra-ui/react';
-import { Progress, Button, Pagination, TabsProps, Tabs, Drawer, Space, Input } from 'antd';
-import { CheckOutlined, EnterOutlined } from '@ant-design/icons';
+import { Progress, Button, Pagination, TabsProps, Tabs, Drawer, Space, Input, Form, Modal } from 'antd';
+import { CheckOutlined, EditOutlined, EnterOutlined } from '@ant-design/icons';
 import { Footer, Header } from "antd/es/layout/layout";
 import Card from "antd/es/card/Card";
 import banner from '/public/start_banner.png';
@@ -15,6 +15,19 @@ function Page() {
   const [tasks, setTasks] = useState<any[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [open, setOpen] = useState(false);
+  const [editingTask, setEditingTask] = useState(null);
+  const [editModalVisible, setEditModalVisible] = useState(false);
+
+  const openEditModal = (task) => {
+    console.log("Editing Task:", task);
+    setEditingTask(task);
+    setEditModalVisible(true);
+  };
+  
+  const closeEditModal = () => {
+    setEditingTask(null);
+    setEditModalVisible(false);
+  };
 
   const onClose = () => {
     setOpen(false);
@@ -136,6 +149,37 @@ function Page() {
     }
   };
 
+  const handleEditTaskDetails = async (event: FormEvent, task) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const taskDetails = JSON.stringify({
+      name: formData.get('name') || task.name,
+      description: formData.get('description') || task.description || "edited task",
+      complete: false,
+      date: formData.get('date') || task.date,
+    });
+    console.log('Edited task:', taskDetails);
+    
+    try {
+      const response = await fetch(`/api/tasks/${editingTask ? editingTask.task_id: "1"}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: taskDetails,
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`Failed to update the task: ${errorData.error}`);
+      }
+      setEditingTask(null);
+      await fetchTasks();
+    } catch (error) {
+      console.error('Error updating task:', error);
+    }
+  };
 
   const renderTaskCard = () => {
     if (activeTab === '1') {
@@ -205,7 +249,26 @@ function Page() {
                 <Button type="primary" style={{ width: '28px', height: '28px', borderRadius: '6px', fontSize: '20px', marginRight: '8px', background: '#0788FF', display: 'flex', justifyContent: 'center', alignItems: 'center' }} icon={<CheckOutlined style={{color: '#262626'}}/>}></Button>
                 <Button type="primary"  onClick={() => moveTasktoToday(task)} style={{ width: '28px', height: '28px', borderRadius: '6px', fontSize: '20px', background: '#B1DFFF', display: 'flex', justifyContent: 'center', alignItems: 'center' }} icon={<EnterOutlined style={{color: '#262626'}}/>}></Button>
                 <div style={{ marginLeft: '12px', display: 'flex', flexDirection: 'column' }}>
-                  <Text style={{ fontFamily: 'Poppins', fontSize: '12px', paddingRight: '92px', whiteSpace: 'nowrap'}}>{task.name}</Text>
+                  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={{ fontFamily: 'Poppins', fontSize: '12px', paddingRight: '4px', whiteSpace: 'nowrap'}}>{task.name}</Text>
+                    <EditOutlined onClick={() => openEditModal(task)} style={{ paddingRight: '92px' }}/>
+                    <Modal
+                      title="Edit Task"
+                      visible={editModalVisible}
+                      onCancel={closeEditModal}
+                      footer={null}
+                    >
+                      <form onSubmit={(event) => handleEditTaskDetails(event, editingTask)}>
+                        <text>Task Name</text>
+                        <Input name="name" defaultValue={editingTask ? editingTask.name : ''} />
+                        <text>Date</text>
+                        <Input name="date" defaultValue={editingTask ? editingTask.date : ''} />
+                        <text>Description</text>
+                        <Input name="description" defaultValue={editingTask ? editingTask.description : ''} />
+                        <Button type="primary" htmlType="submit">Save</Button>
+                      </form>
+                    </Modal>
+                  </div>
                   <hr style={{ width: '120px', marginTop: '4px', borderColor: '#FFF', paddingLeft: '92px', paddingRight: '16px'}} />
                 </div>
               </div>
@@ -223,18 +286,17 @@ function Page() {
     {
       key: '1',
       label: 'Today',
-      // label: <SwitcherOutlined style={{ fontSize: '24px' }}/>,
     },
     {
       key: '2',
       label: 'Later',
-      // label: <UnorderedListOutlined style={{ fontSize: '24px' }}/>,
     },
   ];
   
 
   return <div style={{ backgroundColor: '#FFF', fontFamily: 'Poppins'}}>
-        <Header style={{ display: 'flex', alignItems: 'center' , borderRadius: '0px 0px 20px 20px', height: '176px', backgroundColor: '#D9D9D9'}}>
+
+        <Header style={{ display: 'flex', alignItems: 'center' , borderRadius: '0px 0px 20px 20px', height: '176px', backgroundColor: '#D9D9D9', backgroundImage: `url(${banner})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
             <div>
                 <Text style = {{fontSize: '20px', marginTop: '88px', marginBottom: '7.56px', color: 'black', fontWeight: 'bold'}}>Hello,</Text>
                 <Text style = {{fontSize: '32px', marginTop: '-38px', marginBottom: '34.57px', color: 'black', fontWeight: 'bold'}}>{user?.given_name}</Text>
@@ -242,11 +304,10 @@ function Page() {
         </Header>
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
             <div style={{display: 'flex', flexDirection: 'row', marginTop: '-240px'}}>
-                <Text style= {{fontSize: '24px', fontWeight: 'bolder', marginRight: '28px'}}>Active Tasks</Text>
+                <Text style= {{fontSize: '24px', fontWeight: 'bolder', color: 'black'}}>Active Tasks</Text>
             </div>
             <div>
                 <Tabs defaultActiveKey="1" items={items} onChange={onChange} size= 'large' /> 
-                {/* tabBarStyle={{ fontSize: '24px' , width: '200px'}} */}
             </div>
             <div style={{textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
               {renderTaskCard()}
